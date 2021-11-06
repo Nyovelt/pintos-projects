19e6af4c46595eb80ad6ffa2606f1c99e3d825ca
zhangyn3 <zhangyn3@shanghaitech.edu.cn>
2021-11-06 02:35:42 +0000

    proj setup, keep alarm code

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    uint64_t blocked_ticks;             /* The number of ticks the thread is blocked. */
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
