939dd50dc35e013cef04cb0fd4cffd1601f38d01
zhangyn3 <zhangyn3@shanghaitech.edu.cn>
2021-11-06 03:10:57 +0000

    set up syscall parsing outline

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,6 +15,53 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /*printf ("system call!\n");
+  thread_exit ();*/
+  if (!f->esp)
+    thread_exit ();
+
+  switch(*(int*)f->esp)
+  {
+    case SYS_HALT:
+      printf("syscall halt.\n");
+      break;
+    case SYS_EXIT:
+      printf("syscall exit.\n");
+      break;
+    case SYS_EXEC:
+      printf("syscall exec.\n");
+      break;
+    case SYS_WAIT:
+      printf("syscall wait.\n");
+      break;
+    case SYS_CREATE:
+      printf("syscall create.\n");
+      break;
+    case SYS_REMOVE:
+      printf("syscall remove.\n");
+      break;
+    case SYS_OPEN:
+      printf("syscall open.\n");
+      break;
+    case SYS_FILESIZE:
+      printf("syscall filesize.\n");
+      break;
+    case SYS_READ:
+      printf("syscall read.\n");
+      break;
+    case SYS_WRITE:
+      printf("syscall write.\n");
+      break;
+    case SYS_SEEK:
+      printf("syscall seek.\n");
+      break;
+    case SYS_TELL:
+      printf("syscall tell.\n");
+      break;
+    case SYS_CLOSE:
+      printf("syscall close.\n");
+      break;
+    default:
+      printf("unknown syscall.\n");
+  }
 }
