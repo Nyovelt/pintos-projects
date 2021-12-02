693555e9059bf3c4d186b53ccd3ca0947478efd7
Alex Qin <qinfr@shanghaitech.edu.cn>
2021-12-02 12:41:45 +0000

    update frame.c: switch to hash

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,10 +1,13 @@
-#include <list.h>
+#include <hash.h>
+
+struct hash frame_table; // We use hash_list to store frame table
+
+struct  frame_table_elem{
+    struct hash_elem hash_elem; // hash_elem for hash_list
+    void * frame; // frame pointer
 
 
-struct list frame_table; // We use list to store frame table
-
-struct frames_table_entry
-{
-    struct list_elem elem; // for list
-    void *frame;           // frame address
 };
+
+
+void frame_table_init(void); // Initialize frame table
