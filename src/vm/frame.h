8fe8f1fce87eeeb712a8d4516195b918531c7f84
zhangyn3 <zhangyn3@shanghaitech.edu.cn>
2021-12-04 08:36:06 +0000

    frame struct

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -5,8 +5,9 @@ struct hash frame_table; // We use hash_list to store frame table
 struct  frame_table_elem{
     struct hash_elem hash_elem; // hash_elem for hash_list
     void * frame; // frame pointer
-
-
+    struct thread* owner;
+    struct sup_page_entry* aux;
+    // maybe other info
 };
 
 
