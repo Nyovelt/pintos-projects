503581f32dc89d014b9486a1c1d2ccf6d6e271ad
zhangyn3 <zhangyn3@shanghaitech.edu.cn>
2021-12-04 16:24:53 +0000

    frame get

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,5 +1,7 @@
 #include "vm/frame.h"
 #include "threads/thread.h"
+#include "threads/malloc.h"
+#include "threads/vaddr.h"
 
 static struct hash frame_table;
 static struct lock lock;
@@ -20,14 +22,48 @@ frame_less (const struct hash_elem *a_, const struct hash_elem *b_, void *aux UN
 }
 
 void
-frame_table_init ()
+frame_init ()
 {
   hash_init (&frame_table, frame_hash, frame_less, NULL);
   lock_init (&lock);
 }
 
+void *
+frame_get (enum palloc_flags flags, struct sup_page_entry *upage)
+{
+  /* Validating the address */
+  /* Within 32 bytes of the stack pointer */
+  if (!is_user_vaddr (upage) || !is_user_vaddr ((void *) upage + sizeof (void *) - 1))
+    return NULL;
+
+  lock_acquire (&lock);
+  void *frame = palloc_get_page (flags);
+  if (frame == NULL)
+    {
+      lock_release (&lock);
+      return NULL;
+    }
+
+  struct frame_table_entry *fte = malloc (sizeof (struct frame_table_entry));
+  if (fte == NULL)
+    {
+      palloc_free_page (frame);
+      lock_release (&lock);
+      return NULL;
+    }
+
+  /* Bookeeping for frames */
+  fte->frame = frame;
+  fte->aux = upage;
+  fte->owner = thread_current ();
+  hash_insert (&frame_table, &fte->hash_elem);
+
+  lock_release (&lock);
+  return frame;
+}
+
 struct frame_table_entry *
-frame_table_lookup (const void *frame)
+frame_lookup (const void *frame)
 {
   struct frame_table_entry f;
   struct hash_elem *e;
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -4,11 +4,12 @@
 struct frame_table_entry
 {
     struct hash_elem hash_elem; // hash_elem for hash_list
-    void * frame;                // frame pointer
+    void *frame;                // frame pointer
     struct thread *owner;
     struct sup_page_entry *aux;
     // maybe other info
 };
 
-void frame_table_init (void); // Initialize frame table
-void *frame_get_frame (enum palloc_flags flags, struct sup_page_entry *upage);
+void frame_init (void); // Initialize frame table
+void *frame_get (enum palloc_flags flags, struct sup_page_entry *upage);
+struct frame_table_entry *frame_lookup (const void *frame);
