d13016a7f456d0dcf4945b564473ec5d2ba91dac
zhangyn3 <zhangyn3@shanghaitech.edu.cn>
2021-12-08 05:18:09 +0000

    fix warning

--- src/vm/page.h
+++ src/vm/page.h
@@ -11,9 +11,7 @@ struct sup_page_table_entry
 {
     struct hash_elem hash_elem;
     struct frame_table_entry *frame;
-    void *user_vaddr;     // virtual address of the page
-    void *phys_addr;      // physical address of the page
-    bool present;         // is the page present in memory?
+    const void *vaddr;          // virtual address of the page
     bool writable;        // is the page writable?
     bool swapped;         // is the page swapped?
     uint64_t access_time; // time of last access
@@ -27,7 +25,7 @@ struct sup_page_table_entry
 #endif /* vm/page.h */
 
 void page_init (struct hash *spt);
-bool page_record (struct hash *spt, void *upage, bool writable, struct file *file, off_t ofs, uint32_t read_bytes); // TODO: more args?
-bool page_load (struct hash *spt, void *vaddr, bool write, void* esp);
-void page_free (struct hash *spt, void *vaddr);
+bool page_record (struct hash *spt, const void *upage, bool writable, struct file *file, off_t ofs, uint32_t read_bytes); // TODO: more args?
+bool page_load (struct hash *spt, const void *vaddr, bool write);
+void page_free (struct hash *spt, const void *vaddr);
 bool page_fault_handler (struct hash *spt, const void *addr, bool write, void *esp);
