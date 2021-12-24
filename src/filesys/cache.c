d2a288068ff2f80b9762ea1541c75869ae63d5b6
Alex Qin <qinfr@shanghaitech.edu.cn>
2021-12-24 17:19:10 +0000

    update cache&inode: substitute block_rw
    44/125

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -5,15 +5,28 @@
 
 #define BUF_SIZE 64
 
-struct cache_entry {
-	struct hash_elem hash_elem; // for sector_cache_map
-	block_sector_t sector; // the sector id of the block occupying the cache entry.
-	bool dirty; // for writing back
-	bool used; // for clock algorithm
-	uint8_t data[BLOCK_SECTOR_SIZE];
-	struct rwlock rwlock; // per-entry R/W lock
+struct cache_entry
+{
+    struct hash_elem hash_elem; // for sector_cache_map
+    block_sector_t sector;      // the sector id of the block occupying the cache entry.
+    bool dirty;                 // for writing back
+    bool used;                  // for clock algorithm
+    uint8_t data[BLOCK_SECTOR_SIZE];
+    struct rwlock rwlock; // per-entry R/W lock
 };
 
 static struct cache_entry cache[BUF_SIZE]; // a statically alloc’d array of 64 blocks
-static struct hash sector_cache_map; // a global mapping of sector ids to cache entries
-static struct lock hash_lock; // A global lock to guard the hash-map
+static struct hash sector_cache_map;       // a global mapping of sector ids to cache entries
+static struct lock hash_lock;              // A global lock to guard the hash-map
+
+void
+cache_write (struct block *block, block_sector_t sector, void *buffer)
+{
+  block_write (block, sector, buffer);
+}
+
+void
+cache_read (struct block *block, block_sector_t sector, void *buffer)
+{
+  block_read (block, sector, buffer);
+}
